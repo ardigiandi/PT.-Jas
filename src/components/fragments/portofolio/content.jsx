63f247cadbfe00f047ec9d6dc1b7479f7698f9ca
Ardiansyah Giandi <ardiansyahg9080@gmail.com>
2025-05-24 23:54:39 +0700

    fix : portofolio

--- src/components/fragments/portofolio/content.jsx
+++ src/components/fragments/portofolio/content.jsx
@@ -48,14 +48,17 @@ function CardPortofolio() {
 
       <div className="flex flex-wrap justify-center mt-16 gap-5">
         {CardList.map((card, index) => (
-          <div key={index} className="bg-oren w-[550px] p-5 flex flex-col rounded-md gap-5">
+          <div
+            key={index}
+            className="bg-oren w-[550px] p-5 flex flex-col rounded-md gap-5"
+          >
             <img src={card.image} alt="" />
-            <h2 className="text-xl font-bold text-white">
-              {card.title}
-            </h2>
-            <p className="text-base text-white">
-              {card.description}
-            </p>
+            <div className="space-y-2">
+              <h2 className="text-sm font-medium text-white/80">{card.title}</h2>
+              <h1 className="text-2xl font-bold text-white">Caffe Tutti</h1>
+              <h1 className="text-sm text-white/80 font-medium ">01 Januari 2022</h1>
+            </div>
+            <p className="text-base text-white/80">{card.description}</p>
           </div>
         ))}
       </div>
