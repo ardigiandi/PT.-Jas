03a1307b240bd9c34f21c43063d5045eff1f9a43
Ardiansyah Giandi <ardiansyahg9080@gmail.com>
2025-06-22 17:15:57 +0700

    fix : footer

--- src/components/fragments/footer/footer.jsx
+++ src/components/fragments/footer/footer.jsx
@@ -1,71 +1,49 @@
 import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className="relative w-full bg-oren flex flex-wrap justify-between items-center lg:px-20 gap-12 lg:gap-0 px-5 lg:py-24 py-28">
+    <div className="relative w-full bg-oren flex flex-wrap justify-center items-center lg:px-20 gap-12 lg:gap-0 px-5 lg:py-24 py-28">
       <img
         src="/assets/Titik-titik.png"
         alt=""
         className="absolute top-0 lg:left-14 w-[130px] h-[100px] "
       />
-      <div className="flex flex-col">
-        <img src="/assets/logo-white.png" alt="" className="w-[120px]" />
-        <p className="text-white text-base w-[235px]">
+      <div className="flex flex-col gap-4 justify-center items-center w-full">
+        <ul className="flex gap-5">
+          <li>
+            <a href="/#about" className="text-base text-white">
+              Tentang Kami
+            </a>
+          </li>
+          <li>
+            <a href="/#services" className="text-base text-white">
+              Layanan Kami
+            </a>
+          </li>
+          <li>
+            <a href="/#cta" className="text-base text-white">
+              Kontak Kami
+            </a>
+          </li>
+          <li>
+            <a href="/#portofolio" className="text-base text-white">
+              portofolio
+            </a>
+          </li>
+        </ul>
+
+        <FaWhatsapp className="text-white w-5 h-5" />
+
+        <div className="border-b-[1px] border-white pb-9 w-full">
+          <h1 className="text-white text-[14px] text-center">
+            Jl. Pasar Kecapi No.9 RT.009/RW.016, Jatirahayu, <br /> Kec. Pd.
+            Melati, Kota Bks, Jawa Barat 17414
+          </h1>
+        </div>
+        <p className="text-white text-[14px]">
           Hak Cipta © 2025 PT Jas Seluruh hak cipta dilindungi.
         </p>
-        {/* <div className="flex gap-4 mt-5">
-          <a
-            href="https://wa.me/6287887772919"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/assets/whatsapp.png"
-              alt="WhatsApp"
-              className="w-[27px] h-[27px]"
-            />
-          </a>
-        </div> */}
-      </div>
-      <div className="flex gap-28">
-        <div className="flex flex-col">
-          <h1 className="text-white text-xl font-semibold">Perusahaan</h1>
-          <ul>
-            <li className="flex flex-col gap-2 mt-5">
-              <a href="/#about" className="text-white text-base">
-                Tentang Kami
-              </a>
-              <a href="/#services" className="text-white text-base">
-                Services
-              </a>
-              <a href="/#cta" className="text-white text-base">
-                Kontak Kami
-              </a>
-              <a href="/#portofolio" className="text-white text-base">
-                portofolio
-              </a>
-            </li>
-          </ul>
-        </div>
-        {/* <div className="flex flex-col">
-          <h1 className="text-white text-xl font-semibold">Support</h1>
-          <ul>
-            <li className="flex flex-col gap-2 mt-5">
-              <a href="#" className="text-white text-base">
-                Help center
-              </a>
-              <a href="#" className="text-white text-base">
-                Terms of service
-              </a>
-              <a href="#" className="text-white text-base">
-                Legal
-              </a>
-              <a href="#" className="text-white text-base">
-                Privacy policy
-              </a>
-            </li>
-          </ul>
-        </div> */}
       </div>
       <img
         src="/assets/Meliuk-liuk.png"
